0edf03337cc9474701173cc0d80ffafec4cc3bc1
TianxiongWang <nickwang57@gmail.com>
2015-10-23 00:37:32 +0800

    mfa finalize

--- mfa/mfa.py
+++ mfa/mfa.py
@@ -22,7 +22,7 @@ class MixtureFA(object):
         self.n_mixtures = m
         self.n_factors = k
         self.ll = []
-        self.tol = 1E-6
+        self.tol = 1E-4
         self.iterations = 0
 
     def setup_params(self, X, seed=5):
@@ -33,16 +33,17 @@ class MixtureFA(object):
         self.phi = np.ones(self.n_var)
         self.pi = np.ones(self.n_mixtures) / self.n_mixtures
         self.mu = np.vstack(np.ones(self.n_mixtures)).dot( np.mean(X, axis=0).reshape(1,self.n_var) )
+        self.H = np.zeros((self.sample_size, self.n_mixtures))
 
-    def fit(self, X, cyc = 100):
+    def fit(self, X, cyc = 100,seed=5):
+        np.random.seed(seed)
         tiny = np.exp(-700)
         self.setup_params(X)
-        H = np.zeros((self.sample_size, self.n_mixtures))
         Ez = np.zeros((self.sample_size * self.n_mixtures, self.n_factors))
         Ezz = np.zeros((self.n_factors * self.n_mixtures, self.n_factors))
         s = np.zeros(self.n_mixtures)
 
-        while self.iterations < cyc:
+        while True:
             phi_m = np.diag(1/self.phi)
             for j in xrange(self.n_mixtures):
                 lambdas = self.lambdas[j*self.n_var:(j+1)*self.n_var, :]
@@ -52,33 +53,44 @@ class MixtureFA(object):
                 Xj = X - np.vstack(np.ones(self.sample_size)).dot(self.mu[j,:].reshape(1, self.n_var))
                 XS = Xj.dot(S)
                 Ez[j*self.sample_size:(j+1)*self.sample_size, :] = XS.dot(lambdas)
+                self.H[:,j] = np.power(2*np.pi, -self.n_var/2)*self.pi[j]*dS*np.exp(-0.5*np.diag(XS.dot(Xj.T)))
 
-                beta = lambdas.T.dot(S)
-                Ezz[j*self.n_factors:(j+1)*self.n_factors, :] = np.eye(self.n_factors) - beta.dot(lambdas) + beta.dot(Xj.T.dot(Xj)).dot(beta.T)
-                H[:,j] = np.power(2*np.pi, -self.n_var/2)*self.pi[j]*dS*np.exp(-0.5*np.diag(XS.dot(Xj.T)))
             # remove 0 in H
-            Hrsum = np.sum(H, axis=1)
+            Hrsum = np.sum(self.H, axis=1)
             zero_idx = np.where(Hrsum == 0)
             n_zero = len(zero_idx)
-            H[n_zero, :] = tiny * np.ones((n_zero, self.n_mixtures)) / self.n_mixtures
+            lik = np.sum(np.log(Hrsum + n_zero * tiny))
+
+            self.H[zero_idx, :] = tiny * np.ones((n_zero, self.n_mixtures)) / self.n_mixtures
             Hrsum[zero_idx] = tiny * np.ones(n_zero)
 
-            H = H / np.vstack(Hrsum)
+            self.H = self.H / np.vstack(Hrsum)
             #compute s
-            s = np.sum(H, axis=0)
-            print(s)
+            s = np.sum(self.H, axis=0)
+            # print(s)
 
+            if self.iterations > 0 and ( lik - self.ll[-1] < self.tol ):
+                break
             #compute log likelihood
-            lik = np.sum(np.log(np.sum(H, axis=1)))
             self.ll.append(lik)
+
+
+            for j in xrange(self.n_mixtures):
+                lambdas = self.lambdas[j*self.n_var:(j+1)*self.n_var, :]
+                PL = phi_m.dot(lambdas)
+                S = phi_m - PL.dot(linalg.inv( np.eye(self.n_factors) + np.transpose(lambdas).dot(PL) )).dot(PL.T)
+                Xj = X - np.vstack(np.ones(self.sample_size)).dot(self.mu[j,:].reshape(1, self.n_var))
+                XX = np.transpose( Xj * np.vstack(self.H[:,j]) ).dot(Xj) / s[j]
+                beta = lambdas.T.dot(S)
+                Ezz[j*self.n_factors:(j+1)*self.n_factors, :] = np.eye(self.n_factors) - beta.dot(lambdas) + beta.dot(XX).dot(beta.T)
 
             #compute new params
             ph = np.zeros(self.n_var)
             for j in xrange(self.n_mixtures):
-                l0 = np.vstack(H[:,j]) * X
+                l0 = np.vstack(self.H[:,j]) * X
                 Ez_aug = np.vstack((Ez[j*self.sample_size:(j+1)*self.sample_size, :].T, np.ones(self.sample_size))).T
                 l1 = l0.T.dot(Ez_aug)
-                XH = Ez[j*self.sample_size:(j+1)*self.sample_size, :].T.dot(H[:, j])
+                XH = Ez[j*self.sample_size:(j+1)*self.sample_size, :].T.dot(self.H[:, j])
                 Ezz_aug = np.zeros((self.n_factors+1, self.n_factors+1))
                 Ezz_aug[:self.n_factors,:self.n_factors] = s[j] * Ezz[j*self.n_factors:(j+1)*self.n_factors,:]
                 Ezz_aug[self.n_factors, :self.n_factors] = XH
@@ -95,11 +107,14 @@ class MixtureFA(object):
             self.iterations += 1
 
 if __name__ == "__main__":
+    np.random.seed(5)
     X1 = np.random.multivariate_normal([0,0,0,0],np.eye(4),100)
     X2 = np.random.multivariate_normal([2,2,2,2],np.eye(4),200)
     X = np.vstack((X1, X2))
     mfa = MixtureFA(2, 3)
     mfa.fit(X)
+    print(mfa.iterations)
+    print(mfa.pi)
 
 
 
