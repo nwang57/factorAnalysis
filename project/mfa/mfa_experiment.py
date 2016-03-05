dab6c12c7a1eef0e84a1e9ab66552d227fcf648a
TianxiongWang <nickwang57@gmail.com>
2016-03-05 13:27:42 +0800

    single simulation support on htc

--- project/mfa/mfa_experiment.py
+++ project/mfa/mfa_experiment.py
@@ -7,7 +7,7 @@ from ..modules.simulate import FactorModel
 import os
 import numpy as np
 import csv
-import pdb
+import cPickle
 
 """
     what if we fit a 2 mixtures?
@@ -49,13 +49,14 @@ def cal_ratio(label):
         dic[l] += 1
     return [n / len(label) for n in dic.values()]
 
-def sample_generator(iters, p, size):
+def sample_generator(iters, p, size, seed=None):
     """
         iters : number of simulations
         size : sample size in each simulation
     """
     for i in xrange(iters):
-        X, Y = get_sample(size, p, i)
+        s = seed if seed is not None else i
+        X, Y = get_sample(size, p, s)
         yield X, Y
 
 def mfa_experiment(n_mixtures, output=False):
@@ -103,9 +104,6 @@ def mfa_experiment(n_mixtures, output=False):
             outfile.write(np.array2string(np.mean(np.array(pis), axis=0), separator=',') + '\n')
             # outfile.write("std of Pi\n")
             # outfile.write(np.array2string(np.std(np.array(pis), axis=0), separator=',') + '\n')
-
-def function():
-    pass
 
 def cluster_experiment():
     p = [0.33,0.33,0.34]
@@ -161,6 +159,39 @@ def cluster_experiment():
     np.savetxt(os.path.join('.','result',"distance_dist.csv"), distance_dist, delimiter=',')
     print res
 
+def cluster_simulation(ids, seed):
+    """This will run single simulation with seed and id arguments.
+    The result will be serialized into 2 pickle files, one distance matrix and the other one for detailed result
+    """
+    p = [0.33,0.33,0.34]
+    max_k = 5
+    n_factors = 1
+    result = {'id':ids}
+    for X, Y in sample_generator(1, p, 100, seed = seed):
+        mfa_cluster = MFACluster(X,max_k,n_factors,Y,iters=10)
+        mfa_cluster.fit()
+        result['cv'] = mfa_cluster.best_k("voting")
+        result['ca'] = mfa_cluster.best_k("averaging")
+        result['cstd'] = mfa_cluster.best_k("std")
+
+        min_aic = min_bic = sys.maxint
+        k_aic = k_bic = 0
+        for k in xrange(2, max_k+1):
+            m = MixtureFA(k, n_factors)
+            m.fit(X)
+            result['lik_%d' % k] = m.ll[-1]
+            result['aic_%d' % k] = m.aic()
+            result['bic_%d' % k] = m.bic()
+            if m.aic() < min_aic:
+                k_aic = k
+                min_aic = m.aic()
+            if m.bic() < min_bic:
+                k_bic = k
+                min_bic = m.bic()
+        result['aic'] = k_aic
+        result['bic'] = k_bic
+    cPickle.dump(result, open("pickle/%s_result.p" % ids, 'w'))
+    cPickle.dump(mfa_cluster.result_matrix, open("pickle/%s_matrix.p" % ids, 'w'))
 
 def save_dict_to_csv(result_list):
     with open(os.path.join('.','result',"result2.csv"),'wb') as f:
@@ -170,4 +201,4 @@ def save_dict_to_csv(result_list):
             writer.writerow(result)
 
 if __name__ == "__main__":
-    cluster_experiment()
+    cluster_simulation(1,6)
